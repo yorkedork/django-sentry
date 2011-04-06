4de0e775b4e680a5ad609e8c8dbb66a7f2411065
Thom Linton <thom.linton@gmail.com>
2011-04-05 19:28:36 -0700

    Reverting previous requirements mistake.

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'django-paging>=0.2.2',
-        'django-indexer>=0.2.1',
+        'django-indexer==0.2.1',
         'uuid',
     ],
     dependency_links=[
